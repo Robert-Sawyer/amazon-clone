97f6a7337cf60dbdce8a562bb2f4678b821ab35d
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2023-03-13 16:31:37 +0100

    Added search bar in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
+import { SearchIcon } from '@heroicons/react/solid'
 
 const Header = () =>{
 
     return (
-        <header className='sticky shadow-md top-o z-50 p-5 bg-white grid grid-col-3 md:px-10'>
+        <header className='sticky shadow-md top-o z-50 p-5 bg-white grid grid-cols-3 md:px-10'>
             <div className='relative flex items-center h-10 cursor-pointer w-full'>
                 <Image
                     src='https://1000logos.net/wp-content/uploads/2017/08/Airbnb-logo.jpg'
@@ -12,6 +13,15 @@ const Header = () =>{
                     objectFit='contain'
                     objectPosition='left'
                 />
+            </div>
+
+            <div className='flex items-center py-2 md:border-2 rounded-full md:shadow-sm'>
+                <input
+                    type='text'
+                    placeholder='Start your search'
+                    className='pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400'
+                />
+                <SearchIcon className='hidden md:inline-flex md:mx-2 h-8 bg-red-400 text-white p-2 rounded-full cursor-pointer'/>
             </div>
 
         </header>
