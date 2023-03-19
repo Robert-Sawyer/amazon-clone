14d642619e041bdd79e6ec06c7af896739f92948
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2023-03-19 09:12:02 +0100

    Added key

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -7,6 +7,7 @@ const ProductFeed = ({products}) => {
         <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
             {products?.slice(0,4).map(({id, title, description, price, category, image}) => (
                 <Product
+                    key={id}
                     id={id}
                     title={title}
                     description={description}
@@ -20,6 +21,7 @@ const ProductFeed = ({products}) => {
             <div className='md:col-span-2'>
                 {products?.slice(4, 5).map(({id, title, description, price, category, image}) => (
                     <Product
+                        key={id}
                         id={id}
                         title={title}
                         description={description}
@@ -32,6 +34,7 @@ const ProductFeed = ({products}) => {
 
             {products?.slice(5, products.length).map(({id, title, description, price, category, image}) => (
                 <Product
+                    key={id}
                     id={id}
                     title={title}
                     description={description}
